617eafa51bac7a7a2feefd364d3a5df20491d1ca
Matthew Friend <matthew@lindas-MacBook-Air.local>
2016-07-17 10:52:04 -0500

    First commit

--- InstaPreview/ViewController.swift
+++ InstaPreview/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var siteView: UIWebView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL (string: "https://c99ea5b0ae88d3f864a597e857a5ebfc0f99fd6c.googledrive.com/host/0B6sZpm8Gye1EQUdKaGljNm9JOXc");
+        let requestObj = NSURLRequest(URL: url!);
+        siteView.loadRequest(requestObj);
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
 
